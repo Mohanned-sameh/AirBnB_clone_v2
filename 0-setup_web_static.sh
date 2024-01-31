2b0d9cef62583db6f2a089b7bdfbd56a700c0b36
mohanned-sameh <mohanned2678@gmail.com>
2024-01-31 19:46:05 +0200

    Update setup_web_static.sh and add transfer_file script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static
-sudo apt-get -y update
+# Script that sets up your web servers for the deployment of web_static
+sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
-echo "Holberton School for the win!" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Hello, World!" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-NEW_STRING="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i "38i $NEW_STRING" /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
