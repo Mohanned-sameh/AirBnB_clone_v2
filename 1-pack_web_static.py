73df31466c94bc14b54fdc2fa9fba7a4e762d120
mohanned-sameh <mohanned2678@gmail.com>
2024-01-31 22:11:44 +0200

    Refactor do_pack function to use fabric.api.local and improve file naming

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-from fabric.api import *
-import os
+"""Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack."""
+from fabric.api import local
 from datetime import datetime
-
-env.hosts = ['localhost']
 
 
 def do_pack():
+    """Function to compress files"""
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file = "versions/web_static_{}.tgz".format(date)
     try:
-        filepath = "versions/web_static_" + datetime.now().\
-                   strftime("%Y%m%d%H%M%S") + ".tgz"
-        local("mkdir -p versions")
-        local("tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
-        web_static")
-        print("web_static packed: {} -> {}".
-              format(filepath, os.path.getsize(filepath)))
+        local("tar -cvzf {} web_static".format(file))
+        return file
     except:
-            return None
+        return None
