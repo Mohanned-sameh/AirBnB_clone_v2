053bc3a8a1fcc343f51dccd7443c9eff702746cb
mohanned-sameh <mohanned2678@gmail.com>
2024-01-31 23:16:30 +0200

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,13 +10,11 @@ env.user = "ubuntu"
 def do_pack():
     """Function to compress files"""
     try:
-        if not os.path.exists("versions"):
-            os.mkdir("versions")
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        path = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(path))
-        return path
-    except:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static".format(date))
+        return "versions/web_static_{}.tgz".format(date)
+    except BaseException:
         return None
 
 
@@ -25,16 +23,30 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
-        file = archive_path.split("/")[-1]
-        new_folder = "/data/web_static/releases/" + file.split(".")[0]
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(new_folder))
-        run("tar -xzf /tmp/{} -C {}".format(file, new_folder))
-        run("rm /tmp/{}".format(file))
-        run("mv {}/web_static/* {}/".format(new_folder, new_folder))
-        run("rm -rf {}/web_static".format(new_folder))
+        name = archive_path.split("/")[1].split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run(
+            "tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(
+                name,
+                name,
+            )
+        )
+        run("rm /tmp/{}.tgz".format(name))
+        run(
+            "mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(
+                name, name
+            )
+        )
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(new_folder))
+        run(
+            "ln -s /data/web_static/releases/{}/ \
+                /data/web_static/current".format(
+                name,
+            )
+        )
         return True
-    except:
+    except BaseException:
         return False
