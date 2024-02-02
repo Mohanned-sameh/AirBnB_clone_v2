eb13beb38c5b40abb346d10f2cfd34a6c1394c5a
mohanned-sameh <mohanned2678@gmail.com>
2024-02-02 22:13:48 +0200

    done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -13,13 +13,13 @@ env.user = "ubuntu"
 
 
 def do_pack():
-    """creates an archive from the web_static folder"""
+    """generates a .tgz archive from the contents of the web_static folder"""
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        file_n = "versions/web_static_{}.tgz".format(date)
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        file_name = "versions/web_static_{}.tgz".format(current_time)
         local("mkdir -p versions")
-        local("tar -cvzf {} web_static".format(file_n))
-        return file_n
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
     except Exception as e:
         return None
 
@@ -30,26 +30,26 @@ def do_deploy(archive_path):
         return False
     try:
         put(archive_path, "/tmp/")
-        file_n = archive_path.split("/")[1].split(".")[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(file_n))
+        file_name = archive_path.split("/")[1][:-4]
+        run("mkdir -p /data/web_static/releases/{}/".format(file_name))
         run(
             "tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(
-                file_n, file_n
+                file_name, file_name
             )
         )
-        run("rm /tmp/{}.tgz".format(file_n))
+        run("rm /tmp/{}.tgz".format(file_name))
         run(
             "mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/".format(
-                file_n, file_n
+                file_name, file_name
             )
         )
-        run("rm -rf /data/web_static/releases/{}/web_static".format(file_n))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(file_name))
         run("rm -rf /data/web_static/current")
         run(
-            "ln -s /data/web_static/releases/{}/\
+            "ln -s /data/web_static/releases/{}\
                   /data/web_static/current".format(
-                file_n
+                file_name
             )
         )
         return True
