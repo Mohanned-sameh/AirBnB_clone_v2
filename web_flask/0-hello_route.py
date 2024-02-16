32535f2108403e5577f62d1d31f2269dca2051dc
mohanned-sameh <mohanned2678@gmail.com>
2024-02-17 00:37:25 +0200

    done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
+""" Starts a Flash Web Application """
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,9 +7,10 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Display 'Hello HBNB!'"""
+    """Prints a Message when / is called"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
+    """Main Function"""
     app.run(host="0.0.0.0", port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
+""" Starts a Flash Web Application C is FUN"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,21 +7,22 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Display 'Hello HBNB!'"""
+    """Prints a Message when / is called"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Display 'HBNB'"""
+    """Prints a Message when /hbnb is called"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
-    """Display 'C' followed by the value of the text variable"""
-    return "C {}".format(text.replace("_", " "))
+    """Prints a Message when /c is called"""
+    return "C " + text.replace("_", " ")
 
 
 if __name__ == "__main__":
+    """Main Function"""
     app.run(host="0.0.0.0", port=5000)
